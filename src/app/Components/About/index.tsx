3cfedff4f9402fdee9ff9e3c00b9456496c3ef76
Eduardo Griesang <eduardogriesang1@gmail.com>
2025-04-01 17:23:54 -0300

    feat: Header and About components

---
+++ src/app/Components/About/index.tsx
@@ -0,0 +1,16 @@
+
+export default function About () {
+    return (
+        <section className="text-left max-w-2xl mx-20 justify-start">
+            <h2 className="text-3xl font-bold text-gray-200 mb-6">
+                Are you tired of getting automated rejections from companies?
+            </h2>
+            <span className="text-lg text-gray-200 block mb-2">
+                Maybe your resume doesn't have the right keywords or is not ATS ready!
+            </span>
+            <p className="text-base text-gray-300">
+                Upload your resume with the job description that you're applying for and get it ATS ready right now.
+            </p>
+        </section>
+    )
+}
---
+++ src/app/Components/Header/index.tsx
@@ -0,0 +1,10 @@
+
+export default function Header () {
+    return (
+        <header className="h-96 flex mx-20 items-center">
+            <h1 className="text-center text-9xl font-bold font-mono underline">
+                Resume Improver
+            </h1>
+        </header>
+    )
+}
