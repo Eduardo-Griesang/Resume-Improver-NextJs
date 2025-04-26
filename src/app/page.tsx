009866e78ac41fdfb3ef24c38e210b0dbc75391e
Eduardo Griesang <eduardogriesang1@gmail.com>
2025-04-26 10:34:29 -0300

    feat: changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import Header from "./Components/Header";
 import About from "./Components/About";
+import ResumeUploader from "./Components/ResumeUploader";
 
 export default function Home() {
   //https://matsu-theme.vercel.app
@@ -8,6 +9,7 @@ export default function Home() {
     <>
       <Header />
       <About />
+      <ResumeUploader />
     </>
   );
 }
